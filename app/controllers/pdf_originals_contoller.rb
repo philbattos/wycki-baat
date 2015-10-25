c29c1c0b31ca29b5937f650607342f2d8be5d457
Phil Battos <philbattos@gmail.com>
2015-10-25 01:58:25 -0600

    Start direct-to-S3 uploads

---
+++ app/controllers/pdf_originals_contoller.rb
@@ -0,0 +1,25 @@
+class PdfOriginalsController < ApplicationController
+  before_action :set_s3_direct_post, only: [:new, :index, :create]
+
+  def index
+    @pdf_originals = PdfOriginal.all
+  end
+
+  def new
+    @pdf_original = PdfOriginal.new
+  end
+
+  def create
+    @pdf_original = PdfOriginal.new
+  end
+
+#=================================================
+  private
+#=================================================
+    def set_s3_direct_post
+      @s3_direct_post = S3_BUCKET.presigned_post( key:                    "uploads/#{SecureRandom.uuid}/${filename}",
+                                                  success_action_status:  '201',
+                                                  acl:                    'public-read' )
+    end
+
+end
