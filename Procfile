04f490127402f279dbe32be7d494859563c6a976
Phil Battos <philbattos@gmail.com>
2016-09-16 01:21:39 -0600

    Update Sidekiq settings

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 # web: bundle exec rails server -p $PORT
 
 web: bundle exec puma -t 0:5 -p $PORT
-worker: bundle exec sidekiq -C config/sidekiq.yml -e production
+worker: bundle exec sidekiq -C config/sidekiq.yml -c 10 -e production
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # These settings calculated from: http://manuelvanrijn.nl/sidekiq-heroku-redis-calc/
 
 Sidekiq.configure_client do |config|
-  config.redis = { size: 1, network_timeout: 5 }
+  config.redis = { size: 1, network_timeout: 7 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { size: 5, network_timeout: 5 }
+  config.redis = { size: 9, network_timeout: 7 }
 end
