d0988417e69335483b28807b4e2f1384275a8302
Phil Battos <philbattos@gmail.com>
2020-05-16 23:58:26 -0600

    Reset sidekiq settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,9 @@
 # These settings calculated from: http://manuelvanrijn.nl/sidekiq-heroku-redis-calc/
-#
-# The 'size' setting was removed in 2020 based on this error (in Heroku) and solution:
-#   https://stackoverflow.com/questions/48313434/when-i-increase-sidekiq-concurrency-it-says-my-pool-is-too-small-but-smaller-n
 
 Sidekiq.configure_client do |config|
-  config.redis = { network_timeout: 7 }
+  config.redis = { size: 1, network_timeout: 7 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { network_timeout: 7 }
+  config.redis = { size: 9, network_timeout: 7 }
 end
