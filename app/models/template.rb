d1e13f0a10b6d869c5229e25145497a113dc9b31
Phil Battos <philbattos@gmail.com>
2021-03-03 00:38:23 -0700

    Create subdomains as an ENV variable; Update list of supported subdomains

--- app/models/template.rb
+++ app/models/template.rb
@@ -11,7 +11,7 @@ class Template < ActiveRecord::Base
   validates :name,  presence: true,
                     format: { with: /\A[^.].+$\z/, message: "Template name must not start with a dot(.)" }
   validates :destination, presence: true,
-                          inclusion: { :in => %w[ librarywiki terdzod research ], message: "'%{value}' is not a valid wiki destination. Please enter 'librarywiki', 'terdzod', or 'research'" }
+                          inclusion: { :in => ENV['SUPPORTED_SUBDOMAINS'], message: "'%{value}' is not a valid wiki destination." }
   validates :file_root, presence: true,
                         format: { with: /\ATemplates\z/, message: "The selected folder containing templates should be named 'Templates'. It looks like you selected something else." }
 
